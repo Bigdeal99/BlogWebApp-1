34ad4293ab39992cdf7bde3b54955394548c071b
Marcelo Hani <90682271+Bigdeal99@users.noreply.github.com>
2023-11-24 16:31:44 +0100

    g

--- Api/obj/Release/net8.0/Api.AssemblyInfo.cs
+++ Api/obj/Release/net8.0/Api.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b64ec4b4463e24605f552a0f45c7f9785dd02981")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
